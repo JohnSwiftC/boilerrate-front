4669607c6abf03f3c0fd7f5082e13c9a0e77b254
JohnSwiftC <johnswift@live.com>
2025-09-08 10:50:38 -0400

    build out the beginning of an api helper lib

---
+++ lib/api.tsx
@@ -0,0 +1,34 @@
+interface ApiOptions extends RequestInit {
+  headers?: Record<string, string>
+}
+
+interface CreateUserRequest {
+    email: string,
+    password: string,
+}
+
+interface APICallResponse {
+    ok: boolean,
+    error?: string,
+}
+
+export const createUser = async (req: CreateUserRequest): Promise<APICallResponse> => {
+
+    const request: RequestInit = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req)
+    }
+
+    const response = await fetch(`https://api.boilerrate.com/create_user`, request)
+
+    if (!response.ok) {
+        return {
+            ok: false,
+            error: "Sorry, please try again."
+        }
+    }
+
+    return { ok: true }
+}

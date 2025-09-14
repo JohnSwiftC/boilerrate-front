f98164db16925a96da294a2c590a623164dc3bcc
JohnSwiftC <johnswift@live.com>
2025-09-14 13:08:30 -0400

    suspense boundary hell

--- src/app/updateauth/page.tsx
+++ src/app/updateauth/page.tsx
@@ -1,24 +1,23 @@
 'use client'
 import { useAuth } from '@/../contexts/AuthContext'
-import { useSearchParams } from 'next/navigation'
 import { jwtDecode } from 'jwt-decode'
 import { redirect, RedirectType } from 'next/navigation'
 import { useEffect } from 'react'
 
 export default function UpdateAuth() {
-    const { user } = useAuth()
-    const searchParams = useSearchParams()
     
     useEffect(() => {
-        const token = searchParams.get("token")
-        if (!token || !user) {
+        const urlParams = new URLSearchParams(window.location.search)
+        const token = urlParams.get("token")
+        
+        if (!token) {
             return
         }
-
+        
         localStorage.setItem('token', token)
         
         redirect('/', RedirectType.replace)
-    }, [searchParams, user])
+    }, [])
     
     return <div>Updating authentication...</div>
 }

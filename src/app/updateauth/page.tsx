33dea818937ece09796309e83eca12069c052afa
JohnSwiftC <johnswift@live.com>
2025-09-14 13:06:42 -0400

    actually just need to update the token, the authcontext should handle the user update after the redirect

--- src/app/updateauth/page.tsx
+++ src/app/updateauth/page.tsx
@@ -1,27 +1,24 @@
 'use client'
-
 import { useAuth } from '@/../contexts/AuthContext'
 import { useSearchParams } from 'next/navigation'
 import { jwtDecode } from 'jwt-decode'
 import { redirect, RedirectType } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function UpdateAuth() {
     const { user } = useAuth()
-
     const searchParams = useSearchParams()
-
-    const token = searchParams.get("token")
-    if (!token) {
-        return
-    }
-
-    const decoded: { email: string, conn: string, photo: string | null} = jwtDecode(token)
     
-    if (user) {
-        user.email = decoded.email
-        user.conn = true
-        user.photo = decoded.photo
-    }
+    useEffect(() => {
+        const token = searchParams.get("token")
+        if (!token || !user) {
+            return
+        }
 
-    redirect('/', RedirectType.replace)
+        localStorage.setItem('token', token)
+        
+        redirect('/', RedirectType.replace)
+    }, [searchParams, user])
+    
+    return <div>Updating authentication...</div>
 }
